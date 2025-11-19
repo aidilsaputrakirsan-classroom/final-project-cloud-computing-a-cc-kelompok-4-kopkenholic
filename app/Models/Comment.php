25faa513961e302de6c6d8ef85dea8e728bca6f9
kaylasalbina10221042 <144096567+kaylasalbina10221042@users.noreply.github.com>
2025-11-19 20:23:39 +0800

    Feat: Membuat fitur CRUD Comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,29 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
+    protected $table = "comments";
 
-    /**
-     * Relasi ke Post
-     */
-    public function post()
-    {
-        // Post yang bisa saja terhapus (soft delete)
-        return $this->belongsTo(\App\Models\Post::class, 'post_id')->withTrashed();
+    protected $fillable = [
+        "message",
+        "post_id",
+        "parent_id",
+        "user_id",
+        "name",
+        "email",
+        "status",
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 
-    /**
-     * Relasi ke User (jika ada kolom user_id di tabel comments)
-     */
-    public function user()
-    {
-        return $this->belongsTo(\App\Models\User::class, 'user_id')->withTrashed();
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function replies() {
+        return $this->hasMany($this, "parent_id")->orderBy("created_at", "ASC");
     }
 }
